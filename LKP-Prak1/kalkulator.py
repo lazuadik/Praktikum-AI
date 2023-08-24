ff1c2ac5fdacbef4e743569a7ef5950ae13f263f
lazuardik <andhikarafilazuardi@gmail.com>
2023-08-24 11:47:48 +0700

    LKP-Praktikum1edit

--- LKP-Prak1/kalkulator.py
+++ LKP-Prak1/kalkulator.py
@@ -1,11 +1,12 @@
-angka1 = input()
-operator = input()
-angka2 = input()
-total = eval(angka1 + operator + angka2)
+while(True):
+    angka1 = input()
+    operator = input()
+    angka2 = input()
+    
+    angka1 = int(angka1)
+    angka2 = int(angka2)
 
-if operator == "+":
-    print(angka1, "ditambah", angka2, "sama dengan", total)
-elif operator == "-":
-    print(angka1, "dikurang", angka2, "sama dengan", total)
-else:
-    exit()
+    if operator == "+":
+        print(angka1, "ditambah", angka2, "sama dengan", angka1 + angka2)
+    elif operator == "-":
+        print(angka1, "dikurang", angka2, "sama dengan", angka1 - angka2)
--- LKP-Prak1/tebakbuah.py
+++ LKP-Prak1/tebakbuah.py
@@ -1,20 +1,19 @@
 import random
 list = ["apel", "jeruk", "mangga", "pisang", "durian", "rambutan", "anggur"]
 
-target = random.choice(list)
+while(True):
+    target = random.choice(list)
 
-nyawa = 5
+    nyawa = 5
 
-for i in range(nyawa):
-    tebakan = input("Tebak: ")
-    if tebakan == target:
-        print("Selamat, jawaban Anda benar!")
-        exit()
-    elif tebakan != target:
-        print("Masih salah nih")
-        print("Nyawa Anda tersisa: ", nyawa - 1)
-        nyawa -= 1
-        if nyawa != 0:
-            print("Ayo tebak lagi!")
-        else:
-            exit()
+    for i in range(nyawa):
+        tebakan = input("Tebak: ")
+        if tebakan == target:
+            print("Selamat, jawaban Anda benar!")
+            break
+        elif tebakan != target:
+            print("Salah cuy")
+            print("Nyawa Anda tersisa: ", nyawa - 1, "lagi")
+            nyawa -= 1
+            if nyawa != 0:
+                print("Gas maen lagi!")
