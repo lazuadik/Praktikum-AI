2b79f972114bf1b62e9ebe14770a1adceb99e533
lazuardik <andhikarafilazuardi@gmail.com>
2023-08-27 22:24:50 +0700

    LKP-Praktikum1edit

--- LKP-Prak1/starbek.py
+++ LKP-Prak1/starbek.py
@@ -2,12 +2,12 @@ while(True):
     harga_belanja = int(input("Harga belanja: "))
     if harga_belanja > 50000:
         harga_total = harga_belanja * ((100-27) / 100)
-        print("Selamat! Anda mendapatkan diskon 27%")
+        print("Selamat! Anda dapat diskon 27%")
     elif 30000 <= harga_belanja <= 50000:
         harga_total = harga_belanja * ((100-18) / 100)
-        print("Selamat! Anda mendapatkan diskon 18%")
+        print("Selamat! Anda dapat diskon 18%")
     else:
         harga_total = harga_belanja
-        print("Anda tidak mendapatkan diskon")
+        print("Maap, Anda tidak dapat diskon :(")
 
     print("Harga total belanja Anda: ", "Rp", int(harga_total))
